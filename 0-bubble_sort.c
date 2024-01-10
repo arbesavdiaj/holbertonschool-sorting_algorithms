bfdfe27e6d48c06ed12b52222b5b620d715cd008
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-01-10 12:54:53 +0100

    fix code

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -25,8 +25,8 @@ void bubble_sort(int *array, size_t size)
 				tmp = array[j + 1];
 				array[j + 1] = array[j];
 				array[j] = tmp;
-				print_array(array, size);
 				swapped = 1;
+				print_array(array, size);
 			}
 		}
 		if (swapped == 0)
