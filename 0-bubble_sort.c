5b18ff4bbc8b74cce6b1f08c43987e6e5c02b94c
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-01-10 12:44:24 +0100

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,16 +7,19 @@
  * @size: size of the array
  *
  * Return: void
- */
+*/
 
 void bubble_sort(int *array, size_t size)
 
 {
     size_t i, j, tmp;
 
-    for (i = 0; i < size - 1; i++) {
-        for (j = 0; j < size - i - 1; j++) {
-            if (array[j] > array[j + 1]) {
+    for (i = 0; i < size - 1; i++)
+	{
+        for (j = 0; j < size - i - 1; j++)
+		{
+            if (array[j] > array[j + 1])
+			{
                 tmp = array[j + 1];
 				array[j + 1] = array[j];
 				array[j] = tmp;
