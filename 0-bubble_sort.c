05fcff330a9458fa6b3b887f9236f76007c10a39
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-01-10 12:47:06 +0100

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,19 +12,19 @@
 void bubble_sort(int *array, size_t size)
 
 {
-    size_t i, j, tmp;
+	size_t i, j, tmp;
 
-    for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-        for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-            if (array[j] > array[j + 1])
+			if (array[j] > array[j + 1])
 			{
-                tmp = array[j + 1];
+				tmp = array[j + 1];
 				array[j + 1] = array[j];
 				array[j] = tmp;
 				print_array(array, size);
-            }
-        }
-    }
+			}
+		}
+	}
 }
